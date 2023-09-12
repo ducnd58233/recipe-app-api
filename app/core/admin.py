d755928ded5265f35668d726cc0a8e4c8738cad0
ducnd58233 <only4wns@gmail.com>
2023-09-13 00:52:14 +0700

    fix linting

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,16 +3,19 @@ Django admin customization
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.translation import gettext_lazy as _ # _ make auto translation text
+# _ make auto translation text
+from django.utils.translation import gettext_lazy as _
 
 from core import models
+
 
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-        (None, {'fields': ('email', 'password')}), # No title of the section, 2 fields
+        # No title of the section, 2 fields
+        (None, {'fields': ('email', 'password')}),
         (
             _('Permissions'),
             {
@@ -25,20 +28,21 @@ class UserAdmin(BaseUserAdmin):
         ),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
-    readonly_fields = ['last_login'] # read only field
+    readonly_fields = ['last_login']  # read only field
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': (
-                'email', 
-                'password1', 
-                'password2', 
-                'name', 
-                'is_active', 
-                'is_staff', 
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
                 'is_superuser',
             ),
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,7 +7,7 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
     """Test models."""
-    
+
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
         email = 'test@example.com'
@@ -18,7 +18,7 @@ class ModelTests(TestCase):
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-        
+
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
         sample_email = [
@@ -30,19 +30,17 @@ class ModelTests(TestCase):
         for email, expected in sample_email:
             user = get_user_model().objects.create_user(email, 'test123')
             self.assertEqual(user.email, expected)
-    
+
     def test_new_user_without_email_raises_error(self):
         """Test that creating user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
-    
+
     def test_create_superuser(self):
         """Test creating a superuser."""
         user = get_user_model().objects.create_superuser(
             'test@example.com',
             'test123',
         )
-        
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-        
